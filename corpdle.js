7c2072666338007bbe15c0ee4cb8de647b91e05c
Jason Cartwright <jason@givefood.org.uk>
2022-04-22 16:36:46 +0100

    initial test

---
+++ corpdle.js
@@ -0,0 +1,5 @@
+const csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRZVRc-bGBbM6nu7Ku3N1tkylu8e1qxQr_rSAEYJFG8CAa4oOS6lShkKb1CtVoLaxn4LwoEG-GI_G0m/pub?output=csv"
+
+Papa.parse(csv_url, {
+	download: true
+})
