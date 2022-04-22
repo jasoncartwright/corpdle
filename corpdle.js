084e041d00ef265048feb1ba075cb0e98d08fc24
Jason Cartwright <jason@givefood.org.uk>
2022-04-22 16:43:00 +0100

    test csv parsing

--- corpdle.js
+++ corpdle.js
@@ -1,5 +1,9 @@
 const csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRZVRc-bGBbM6nu7Ku3N1tkylu8e1qxQr_rSAEYJFG8CAa4oOS6lShkKb1CtVoLaxn4LwoEG-GI_G0m/pub?output=csv"
 
 Papa.parse(csv_url, {
-	download: true
+	download: true,
+    header: true,
+    complete: function(results, file) {
+        console.log("Parsing complete:", results, file);
+    }
 })
